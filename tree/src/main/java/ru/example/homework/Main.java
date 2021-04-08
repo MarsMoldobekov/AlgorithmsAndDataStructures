f16801f52e8a4d7e70f3d741e779127eb8485d31
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-04-08 16:36:53 +0300

    Implement heapsort

--- tree/src/main/java/ru/example/homework/Main.java
+++ tree/src/main/java/ru/example/homework/Main.java
@@ -1,5 +1,7 @@
 package ru.example.homework;
 
+import java.util.Arrays;
+
 public class Main {
     private static final Person[] people = {
             new Person("Nikita", 1, 20),
@@ -18,6 +20,8 @@ public class Main {
             3, 8, 2, 0, 4, 6, 1, 7, 9, 5
     };
 
+    private static int heapSize;
+
     private static final Integer integer = 8;
     private static final Person individual = new Person("Natasha", 9, 19);
 
@@ -27,6 +31,12 @@ public class Main {
 
         System.out.println("---TREE OF PEOPLE---");
         makeTree(people, individual);
+
+        System.out.println("---Origin array---");
+        System.out.println(Arrays.toString(integers));
+        System.out.println("---Sorting an array by heapsort---");
+        heapsort(integers);
+        System.out.println(Arrays.toString(integers));
     }
 
     private static <T extends Comparable<? super T>> void makeTree(T[] array, T element) {
@@ -49,5 +59,56 @@ public class Main {
 
         System.out.println("PRE_ORDER");
         integerTree.display(Tree.DisplayType.PRE_ORDER);
+    }
+
+    private static void heapsort(Integer[] array) {
+        buildHeap(array);
+
+        while (heapSize > 1) {
+            swap(0, heapSize - 1, array);
+            heapSize--;
+            heapify(0, array);
+        }
+    }
+
+    private static void buildHeap(Integer[] array) {
+        heapSize = array.length;
+
+        for (int i = array.length / 2; i >= 0; i--) {
+            heapify(i, array);
+        }
+    }
+
+    private static void heapify(int i, Integer[] array) {
+        int left = getLeft(i);
+        int right = getRight(i);
+        int largest = i;
+
+        if (left < heapSize && array[i] < array[left]) {
+            largest = left;
+        }
+
+        if (right < heapSize && array[largest] < array[right]) {
+            largest = right;
+        }
+
+        if (i != largest) {
+            swap(i, largest, array);
+            heapify(largest, array);
+        }
+    }
+
+    private static int getLeft(int i) {
+        return 2 * i + 1;
+    }
+
+    private static int getRight(int i) {
+        return 2 * i + 2;
+    }
+
+    private static void swap(int i, int j, Integer[] array) {
+        Integer temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
